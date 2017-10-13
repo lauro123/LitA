b88d9688730a78de954d12e5add4da2a5c34b5f2
Lauro123 <laurocaccia@gmail.com>
2017-10-13 10:23:24 -0300

    localization with agularjs

--- localization.js
+++ localization.js
@@ -1,14 +1,13 @@
 var userLang = navigator.language || navigator.userLanguage;
-//alert("The language is: " + userLang);
-var elements = document.getElementsByClassName('localize');
 
-for (var i = 0; i < elements.length; i++) {
-    elements[i].innerHTML = "foo";
-}​
-if (userLang == 'es')
-{
-
-} else 
-{
-
-}
+var indexAPP = angular.module('index', []);
+indexAPP.controller('indexController', function ($scope) {
+    if(userLang ="es")
+    {
+        $scope.home = "Inicio";
+    }
+    else
+    {
+        $scope.home = "Home";
+    }
+});
